114a2c7ddd7ac397f0ec7e16857ae6063b0e69dc
FSJessica <fjes.ks@gmail.com>
2023-05-11 13:23:43 -0300

    Initial commit

---
+++ palavra_secreta.py
@@ -0,0 +1,36 @@
+import random
+palavras_secretas = ['amarelo', 'abacaxi', 'coelho', 'lavanda', 'bola', 'juiz', 'cabelo', 'vidro', 'estranho', 'foto',
+                     'morcego', 'aumentar', 'revendedor', 'diagonal', 'labirinto', 'trincheira', 'corrida', 'ator',
+                     'pia', 'agricultor','uva', 'goiaba', 'jardim']
+
+palavra_secreta = random.choice(palavras_secretas)
+letras_acertadas= ''
+numero_tentativas = 0
+
+while True:
+    letra_digitada = input('Digite uma letra: ')
+    numero_tentativas+= 1
+
+    if len(letra_digitada) >1:
+        print('Digite apenas uma letra.')
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+
+
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += '_'
+    print('Palavra formada: ', palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        print('VOCÊ GANHOU! PARABÉNS!')
+        print('A palavra era', palavra_secreta)
+        print('Tentativas: ', numero_tentativas)
+        letras_acertadas = ''
+        numero_tentativas = 0
+        break
